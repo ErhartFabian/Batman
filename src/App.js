9183c7214d8ab966a3de683059d8c9982f9db186
ErhartFabian <A01274089@itesm.mx>
2021-10-02 15:19:22 -0500

    import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import * as React from 'react';
+import React,{useState, useRef} from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@mui/material/Button';
 
 function App() {
+  const [status,setStatus] = useState(); ///
+  
   return <Button variant="contained">Hello World</Button>;
 }
 
