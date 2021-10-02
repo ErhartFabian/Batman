67d4ecf4dcae4a363961522a554bf33c730d0f17
ErhartFabian <A01274089@itesm.mx>
2021-10-02 16:42:58 -0500

    structure

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import Button from '@mui/material/Button';
 
 function App() {
-  const [status,setStatus] = useState(); ///
+  const [status,setStatus] = useState('initial'); ///
   
   return <Button variant="contained">Hello World</Button>;
 }
+
+
 
 export default App;
