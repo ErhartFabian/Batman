5ee65568a6b0d42df67d1ae2e36b5e9be920e135
ErhartFabian <A01274089@itesm.mx>
2021-10-02 12:15:42 -0500

    Material UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import React,{useState, useRef} from 'react';
-import './App.css';
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <Button variant="contained">Hello World</Button>;
 }
 
 export default App;
