3511d1e13aa937e3a36f895b916eba3696accf43
ErhartFabian <A01274089@itesm.mx>
2021-10-03 13:57:05 -0500

    style and buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import React,{useState, useRef} from 'react';
 import ReactDOM from 'react-dom';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button'
 
 export function Form(){
 
   return (
     <div id="form">
       <h2>Fill the form</h2>
+      <div id="fill">
+        <div id="fill_left">
+          <TextField id="standard-basic" label="To" variant="standard" />
+          <TextField id="standard-basic" label="From" variant="standard" />
+          <TextField id="standard-basic" label="Action Requring Apology" variant="standard" />
+          <TextField id="standard-basic" label="Select Option" variant="standard" />
+          <TextField id="standard-basic" label="Consequence of your action" variant="standard" />
+          <TextField id="standard-basic" label="Your Intention" variant="standard" />
+        </div>
+        <div id="fill_right">
+          <TextField id="standard-basic" label="Percived Motive" variant="standard" />
+          <TextField id="standard-basic" label="Adjetive" variant="standard" />
+          <TextField id="standard-basic" label="Proposed Solution" variant="standard" />
+          <TextField id="standard-basic" label="Request for other party" variant="standard" />
+          <TextField id="standard-basic" label="Relationship to reason" variant="standard" />
+          <TextField id="standard-basic" label="Your name" variant="standard" />
+        </div>
+      </div>
+      < div id="fill_buttons">
+        <Button variant="contained" className="btn" sx={{
+          width: 200,
+          backgroundColor: 'red',
+          marginTop:'40px',
+          marginRight:'30px',
+        }}>Reset</Button>
+        <Button variant="contained"sx={{
+          width: 200,
+          backgroundColor: 'Green',
+          marginTop:'40px',
+          marginLeft:'30px',
+        }} >Generate</Button></div>
+        
     </div>
   )
 
@@ -35,7 +68,7 @@ export function Letter(){
   const val = 'HELLO';
   return(
     <div id="letter">
-      <p>It has come to my attention that my action of {val} colud be seen as I neves intended to . I want you to understand that I was merely trying to, tough I can see now that it may appear that I was . Please accept my apology. Moving forward, I will attemp to. That said, I would very much apreciate it if you. Sincerely, your,.</p>
+      <p>It has come to my attention that my action of (1){} colud be seen as(2){} I never intended to (3){}. I want you to understand that I was merely trying to(4){}, tough I can see now that it may appear that I was (5){}. Please accept my (6){} apology. Moving forward, I will attemp to(7){}. That said, I would very much apreciate it if you (8){}. Sincerely, (9){} your, (10){}.</p>
     </div>
   )
 }
