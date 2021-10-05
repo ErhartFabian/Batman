090f03bd74042ff4b6ab92fd3b2de1bfbd5f4578
ErhartFabian <A01274089@itesm.mx>
2021-10-05 18:42:24 -0500

    solucion problema con form y onSubmit

--- src/App.js
+++ src/App.js
@@ -103,8 +103,7 @@ export function Form(){
     ];
 
 
-  const [activar, setActivar] = useState(false); //Para deshabilitar las inputs
-  const [status, setStatus] = useState('initial'); // check when status completed
+  const [activar, setActivar] = useState(false); //Para deshabilitar las inputs y checar status
   const [answers, setAnswers] = useState({ //Objeto con las respuestas
     uno: "",
     dos: "",
@@ -125,17 +124,15 @@ export function Form(){
       ...answers,
       [event.target.id]: event.target.value
     })
+
+
   };
 
   function handleSubmit(event) {
     event.preventDefault();// evita comportamiento default del form
-  }
-
-  function fill(){
-    setStatus("complete");
     setActivar(true);
   }
-
+  
   function random(){
     const fromN = Math.floor(Math.random()*6);
     setAnswers({
@@ -156,7 +153,6 @@ export function Form(){
 
   function reset(){
     // asignar valores inputs a campos vacios
-   setStatus('initial');
    setActivar(false);
    setAnswers({
      uno: "",
@@ -174,53 +170,65 @@ export function Form(){
    })
   }
 
+  function print(){
+    
+  }
+
 
   return (
-    <div id="form" onSubmit={handleSubmit}>
+    <div>
+    <form onSubmit={handleSubmit}>
       <h2>Fill the form</h2>
-      <div id="fill">
-        <div id="fill_left">
-          <TextField id="uno" label="To" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.uno}/>
-          <TextField id="dos" label="From" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.dos}/>
-          <TextField id="tres" label="Action Requring Apology" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.tres}/>
-          <TextField id="cuatro" label="Select Option" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.cuatro}/>
-          <TextField id="cinco" label="Consequence of your action" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.cinco}/>
-          <TextField id="seis" label="Your Intention" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.seis}/>
+        <div id="fill">
+          <div id="fill_left">
+            <TextField id="uno" label="To" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.uno}/>
+            <TextField id="dos" label="From" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.dos}/>
+            <TextField id="tres" label="Action Requring Apology" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.tres}/>
+            <TextField id="cuatro" label="Select Option" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.cuatro}/>
+            <TextField id="cinco" label="Consequence of your action" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.cinco}/>
+            <TextField id="seis" label="Your Intention" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.seis}/>
+          </div>
+          <div id="fill_right">
+            <TextField id="siete" label="Percived Motive" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.siete}/>
+            <TextField id="ocho" label="Adjetive" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.ocho}/>
+            <TextField id="nueve" label="Proposed Solution" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.nueve}/>
+            <TextField id="diez" label="Request for other party" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.diez}/>
+            <TextField id="once" label="Relationship to reason" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.once}/>
+            <TextField id="doce" label="Your name" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.doce}/>
+          </div>
         </div>
-        <div id="fill_right">
-          <TextField id="siete" label="Percived Motive" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.siete}/>
-          <TextField id="ocho" label="Adjetive" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.ocho}/>
-          <TextField id="nueve" label="Proposed Solution" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.nueve}/>
-          <TextField id="diez" label="Request for other party" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.diez}/>
-          <TextField id="once" label="Relationship to reason" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.once}/>
-          <TextField id="doce" label="Your name" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.doce}/>
-        </div>
-      </div>
-
-      < div id="fill_buttons">
-        <Button onClick={reset} variant="contained" className="btn" sx={{
-          width: 200,
-          backgroundColor: 'red',
-          marginTop:'40px',
-          marginRight:'30px',
-        }}>Reset</Button>
-        <Button onClick={random} variant="contained"sx={{
-          width: 200,
-          backgroundColor: 'Green',
-          marginTop:'40px',
-          marginLeft:'30px',
-        }} >Random</Button>
       
-        <Button onClick={fill} variant="contained"sx={{ //Debería ser onSubmit
-          width: 200,
-          backgroundColor: 'Blue',
-          marginTop:'40px',
-          marginLeft:'30px',
-        }} >Generate</Button>
+        < div id="fill_buttons">
+          <Button onClick={reset} variant="contained" className="btn" sx={{
+            width: 200,
+            backgroundColor: 'red',
+            marginTop:'40px',
+            marginRight:'30px',
+          }}>Reset</Button>
+          <Button onClick={random} variant="contained"sx={{
+            width: 200,
+            backgroundColor: 'Green',
+            marginTop:'40px',
+            marginLeft:'30px',
+          }} >Random</Button>
+        
+          <Button type="submit" variant="contained"sx={{ //Debería ser onSubmit
+            width: 200,
+            backgroundColor: 'Blue',
+            marginTop:'40px',
+            marginLeft:'30px',
+          }} >Generate</Button>
+          <Button  variant="contained"sx={{ //Debería ser onSubmit
+            width: 200,
+            backgroundColor: 'Gray',
+            marginTop:'40px',
+            marginLeft:'30px',
+          }} >Print</Button>
 
-        </div>
+          </div>
+        </form>
 
-        <div id="title" className={status === "complete" ? "show" : "hide"}>
+        <div id="title" className={activar === true ? "show" : "hide"}>
           <div id="title_top">
             <h2>FORMAL APOLOGY</h2>
           </div>
@@ -234,50 +242,15 @@ export function Form(){
           </div>
         </div>
         
-        <div id="letter" className={status === "complete" ? "show" : "hide"}>
-           <p>It has come to my attention that my action of (1){answers.tres} colud be seen as(2){answers.cuatro} I never intended to (3){answers.cinco}. 
-           I want you to understand that I was merely trying to(4){answers.seis}, tough I can see now that it may appear that I was (5){answers.siete}. 
-           Please accept my (6){answers.ocho} apology. Moving forward, I will attemp to(7){answers.nueve}. That said, I would very much apreciate it if you (8){answers.diez}. 
-           Sincerely, (9){answers.once} your, (10){answers.doce}.</p>
+        <div id="letter" className={activar === true ? "show" : "hide"}>
+           <p>It has come to my attention that my action of <b>(1){answers.tres}</b> colud be seen as <b>(2){answers.cuatro}</b> I never intended to <b>(3){answers.cinco}</b>. 
+           I want you to understand that I was merely trying to <b>(4){answers.seis}</b>, tough I can see now that it may appear that I was <b>(5){answers.siete}</b>. 
+           Please accept my <b>(6){answers.ocho}</b> apology. Moving forward, I will attemp to <b>(7){answers.nueve}</b>. That said, I would very much apreciate it if you <b>(8){answers.diez}</b>. 
+           Sincerely, your <b>(9){answers.once}</b>, <b>(10){answers.doce}</b>.</p>
         </div>
 
-    </div>
+        </div>
   )
 }
-  
-// function Header() {
-  
-//   return (
-//     <div id="title">
-//       <div id="title_top">
-//         <h2>FORMAL APOLOGY</h2>
-//       </div>
-//       <div id="title_down"> 
-//         <div id="title_down_left">
-//           <label>To: {answers.uno}</label>{}
-//         </div>
-//         <div id="title_down_right">
-//           <label>From:</label>
-//         </div>
-//       </div>
-//     </div>
-    
-//   );
-// }
-
-
-
-// export function Letter(){
-//   const val = 'HELLO';
-//   return(
-//     <div id="letter">
-//       <p>It has come to my attention that my action of (1){} colud be seen as(2){} I never intended to (3){}. I want you to understand that I was merely trying to(4){}, tough I can see now that it may appear that I was (5){}. Please accept my (6){} apology. Moving forward, I will attemp to(7){}. That said, I would very much apreciate it if you (8){}. Sincerely, (9){} your, (10){}.</p>
-//     </div>
-//   )
-// }
-
-
-
-// export default Header;
 
 export default Form;
