6a5d30175c61d598af6b4fa77ab0fce78fa624c5
ErhartFabian <A01274089@itesm.mx>
2021-10-06 13:50:51 -0500

    cambio de nombre de variable a status

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ export function Form(){
     ];
 
 
-  const [activar, setActivar] = useState(false); //Para deshabilitar las inputs y checar status
+  const [status, setStatus] = useState(false); //Para deshabilitar las inputs y checar status
   const [answers, setAnswers] = useState({ //Objeto con las respuestas
     uno: "",
     dos: "",
@@ -130,7 +130,7 @@ export function Form(){
 
   function handleSubmit(event) {
     event.preventDefault();// evita comportamiento default del form
-    setActivar(true);
+    setStatus(true);
   }
   
   function random(){
@@ -153,7 +153,7 @@ export function Form(){
 
   function reset(){
     // asignar valores inputs a campos vacios
-   setActivar(false);
+   setStatus(false);
    setAnswers({
      uno: "",
      dos: "",
@@ -181,20 +181,20 @@ export function Form(){
       <h2>Fill the form</h2>
         <div id="fill">
           <div id="fill_left">
-            <TextField id="uno" label="To" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.uno}/>
-            <TextField id="dos" label="From" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.dos}/>
-            <TextField id="tres" label="Action Requring Apology" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.tres}/>
-            <TextField id="cuatro" label="Select Option" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.cuatro}/>
-            <TextField id="cinco" label="Consequence of your action" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.cinco}/>
-            <TextField id="seis" label="Your Intention" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.seis}/>
+            <TextField id="uno" label="To" variant="standard" type="text" onChange={handleChange} disabled={status} value={answers.uno}/>
+            <TextField id="dos" label="From" variant="standard" type="text" onChange={handleChange} disabled={status} value={answers.dos}/>
+            <TextField id="tres" label="Action Requring Apology" variant="standard" type="text" onChange={handleChange} disabled={status} value={answers.tres}/>
+            <TextField id="cuatro" label="Select Option" variant="standard" type="text" onChange={handleChange} disabled={status} value={answers.cuatro}/>
+            <TextField id="cinco" label="Consequence of your action" variant="standard" type="text" onChange={handleChange} disabled={status} value={answers.cinco}/>
+            <TextField id="seis" label="Your Intention" variant="standard" type="text" onChange={handleChange} disabled={status} value={answers.seis}/>
           </div>
           <div id="fill_right">
-            <TextField id="siete" label="Percived Motive" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.siete}/>
-            <TextField id="ocho" label="Adjetive" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.ocho}/>
-            <TextField id="nueve" label="Proposed Solution" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.nueve}/>
-            <TextField id="diez" label="Request for other party" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.diez}/>
-            <TextField id="once" label="Relationship to reason" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.once}/>
-            <TextField id="doce" label="Your name" variant="standard" type="text" onChange={handleChange} disabled={activar} value={answers.doce}/>
+            <TextField id="siete" label="Percived Motive" variant="standard" type="text" onChange={handleChange} disabled={status} value={answers.siete}/>
+            <TextField id="ocho" label="Adjetive" variant="standard" type="text" onChange={handleChange} disabled={status} value={answers.ocho}/>
+            <TextField id="nueve" label="Proposed Solution" variant="standard" type="text" onChange={handleChange} disabled={status} value={answers.nueve}/>
+            <TextField id="diez" label="Request for other party" variant="standard" type="text" onChange={handleChange} disabled={status} value={answers.diez}/>
+            <TextField id="once" label="Relationship to reason" variant="standard" type="text" onChange={handleChange} disabled={status} value={answers.once}/>
+            <TextField id="doce" label="Your name" variant="standard" type="text" onChange={handleChange} disabled={status} value={answers.doce}/>
           </div>
         </div>
       
@@ -228,7 +228,7 @@ export function Form(){
           </div>
         </form>
 
-        <div id="title" className={activar === true ? "show" : "hide"}>
+        <div id="title" className={status === true ? "show" : "hide"}>
           <div id="title_top">
             <h2>FORMAL APOLOGY</h2>
           </div>
@@ -242,7 +242,7 @@ export function Form(){
           </div>
         </div>
         
-        <div id="letter" className={activar === true ? "show" : "hide"}>
+        <div id="letter" className={status === true ? "show" : "hide"}>
            <p>It has come to my attention that my action of <b>(1){answers.tres}</b> colud be seen as <b>(2){answers.cuatro}</b> I never intended to <b>(3){answers.cinco}</b>. 
            I want you to understand that I was merely trying to <b>(4){answers.seis}</b>, tough I can see now that it may appear that I was <b>(5){answers.siete}</b>. 
            Please accept my <b>(6){answers.ocho}</b> apology. Moving forward, I will attemp to <b>(7){answers.nueve}</b>. That said, I would very much apreciate it if you <b>(8){answers.diez}</b>. 
